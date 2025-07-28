7a54090f4af7d3ae61f16286f16e453399c5a174
Murembiwa Mutswaletswale <murembiwanoz64@gmail.com>
2025-07-29 00:33:12 +0200

    Create practical solutions.sql

---
+++ solutions/practical solutions.sql
@@ -0,0 +1,14 @@
+/*Practical solutions
+1. If communities are using rivers, we can dispatch trucks to those regions to provide water temporarily in the short term, while we send out
+crews to drill for wells, providing a more permanent solution.
+2. If communities are using wells, we can install filters to purify the water. For wells with biological contamination, we can install UV filters that
+kill microorganisms, and for *polluted wells*, we can install reverse osmosis filters. In the long term, we need to figure out why these sources
+are polluted.
+3. For shared taps, in the short term, we can send additional water tankers to the busiest taps, on the busiest days. We can use the queue time
+pivot table we made to send tankers at the busiest times. Meanwhile, we can start the work on installing extra taps where they are needed.
+According to UN standards, the maximum acceptable wait time for water is 30 minutes. With this in mind, our aim is to install taps to get
+queue times below 30 min.
+4. Shared taps with short queue times (< 30 min) represent a logistical challenge to further reduce waiting times. The most effective solution,
+installing taps in homes, is resource-intensive and better suited as a long-term goal.
+5. Addressing broken infrastructure offers a significant impact even with just a single intervention. It is expensive to fix, but so many people
+can benefit from repairing one facility. For example, fixing a reservoir or pipe that multiple taps are */
